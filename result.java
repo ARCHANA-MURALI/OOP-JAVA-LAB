c9013e31c4cf7af487f578e149e4e3fbd51580c0
ARCHANA-MURALI <117839833+ARCHANA-MURALI@users.noreply.github.com>
2023-06-26 15:40:38 +0530

    Update result.java

--- result.java
+++ result.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
 class sports{
     String sport;
-    int Rating;
-    sports(String spo, int ra){
+    int score;
+    sports(String spo, int sc){
         sport = spo;
-        Rating = ra;
+        score = sc;
     }
 
 }
 class student extends sports{
     String Grade;
     double Overall_per;
-    student(String spo, int ra,String gd, double per ){
-        super(spo, ra);
+    student(String spo, int sc,String gd, double per ){
+        super(spo, sc);
         Grade = gd;
         Overall_per = per;
     }
 }
 public class result extends student {
-    result(String spo, int ra,String gd, double per ){
-        super(spo, ra, gd, per);
+    result(String spo, int sc,String gd, double per ){
+        super(spo, sc, gd, per);
     }
     void display(){
         System.out.println("\nSports Details of Student");
-        System.out.println("Sport :"+sport);
-        System.out.println("Rating :"+Rating);
+        System.out.println(" Name of Sports :"+sport);
+        System.out.println("sports score:"+score);
         System.out.println("\nAcademic Details of Student");
         System.out.println("Academic Grade :"+Grade);
         System.out.println("Overall percentage :"+Overall_per);
@@ -37,7 +37,7 @@ public class result extends student {
         System.out.println("\nEnter the Sports Details of Student");
         System.out.println("\n Sport: ");
         String a =sc.next();
-        System.out.println("\n Sport Rating  out of 10: ");
+        System.out.println("\n Sport score out of 10: ");
         int b =sc.nextInt();
         System.out.println("\nEnter the Sports Details of Student");
         System.out.println("\n Academic Grade: ");
